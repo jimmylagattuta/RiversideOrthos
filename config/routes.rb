34892d31020d254c3b27324dd3d984475554f15f
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-07-26 10:55:44 -0700

    react configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :notes, only: [:index]
+
+      get "/me", to: "users#show"
+      post "/signup", to: "users#create"
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
+    end
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
