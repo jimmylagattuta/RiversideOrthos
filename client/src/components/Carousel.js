fe016f523e887cb1bb088b1db78cc710bafcd8f7
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-08-03 09:08:50 -0700

    updated banner photo

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 const ReactBackgroundCarousel = ({ children }) => {
+    console.log('children', children);
     return (
         <div className='Carousel-container'>
-            Carousel.js
+            <img
+                src={children.props.src}
+                alt={children.props.alt}
+                style={{ width: '1485px', height: '696px' }}
+                className='carousel-img'
+                loading='eager'
+            />
         </div>
     );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -47,11 +47,6 @@ const Home = () => {
                     <p style={{ color: 'black' }} className='banner-description'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate sagittis velit, id convallis tellus aliquet eu. Ut sem felis, fermentum a dui nec, faucibus malesuada elit. Integer euismod, ligula nec ullamcorper bibendum, enim felis tristique nunc, at luctus nisl eros non erat. Sed vel augue ac quam tristique vulputate. Proin ac bibendum elit. Sed ut lectus euismod, hendrerit velit et, interdum justo. Donec egestas euismod mauris, vel efficitur nunc fringilla non. Vivamus quis ante vitae odio pharetra eleifend.
 
-                        Maecenas id dapibus nulla. Cras volutpat est et turpis consectetur, eu convallis elit efficitur. Maecenas bibendum nisi in enim pharetra, eu convallis risus dictum. Fusce ut ullamcorper urna, id vestibulum ligula. Pellentesque id tellus eget mi faucibus tincidunt vel vel tortor. Integer et elit et tortor venenatis sollicitudin in sit amet tellus. Nam id facilisis neque. Suspendisse sed elit vestibulum, volutpat mauris eget, tristique mi. Integer vitae fermentum nisl.
-
-                        Aliquam erat volutpat. Suspendisse non odio eget orci venenatis mattis eu sit amet mi. Suspendisse quis nulla a dui euismod venenatis. Vestibulum elementum odio eget libero ultrices, vel dictum elit gravida. Suspendisse potenti. Aenean volutpat metus sit amet tortor aliquam, ut venenatis justo consectetur. Nullam faucibus, nisi non vestibulum laoreet, ipsum enim venenatis urna, in suscipit odio sapien nec enim. Donec ac tristique erat. Vestibulum consectetur nisi ac posuere faucibus.
-
-                        Duis rhoncus leo sit amet sapien hendrerit pharetra. Nullam malesuada metus eu lorem bibendum, in gravida tortor fringilla. Vestibulum cursus sapien ac aliquet malesuada. Proin nec velit risus. Vestibulum bibendum, nisl vitae hendrerit consectetur, lectus nisl ullamcorper elit, sit amet blandit urna sem quis libero. Donec condimentum ac ex vel pharetra. Nam et libero nec turpis suscipit blandit a vitae urna.
 
                     </p>
                     <div className='banner-buttons'>
@@ -71,7 +66,14 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <ReactBackgroundCarousel />                   
+                <ReactBackgroundCarousel>
+                    <img
+                        src='LaOrtho1.png'
+                        alt='img1'
+                        className={'carousel-img'}
+                        style={{ width: '1485px', height: '696px' }}
+                    />
+                </ReactBackgroundCarousel>                   
             </div>
             <div className='home-expertise'>
                 <h2 className='section-title'>Areas of Expertise</h2>
