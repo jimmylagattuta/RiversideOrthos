324aa8e6ce78a6f8ef1bea32d7cb33567ee7317b
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-09-30 07:45:13 -0700

    testing reviews

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'
+      origins 'https://la-orthos-bdc751615c67.herokuapp.com'
+    #   origins 'http://localhost:3001'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put]
     end
   end
