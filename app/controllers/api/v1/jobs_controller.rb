09b95f35a1be9d8813bd6b29bad0a82fa2bafe75
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-09-30 08:36:39 -0700

    testing reviews

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -12,35 +12,42 @@ class Api::V1::JobsController < ApplicationController
       puts "api_key"
       puts api_key
       puts "*" * 100
+      puts "1"
       if api_key.nil? || api_key.empty?
-        puts "nil"
+        puts "2"
         render json: { "error": "Please set the GOOGLE_PLACES_API_KEY environment variable." }
         return
       end
-  
+      puts "3"
       places_api = GooglePlacesApi.new(api_key)
-  
+      puts "4"
       # Replace 'YOUR_PLACE_ID_TO_LOOKUP' with an actual place ID to retrieve details
       place_id = 'ChIJ6wjoflfGwoARIQ4pYyXJCN8'
+      puts "5"
       encoded_place_id = URI.encode_www_form_component(place_id)
+      puts "6"
       place_details = places_api.get_place_details(encoded_place_id)
-  
       if place_details
+        puts "7"
         render json: place_details
       else
+        puts "8"
         render json: { "error": "Failed to retrieve place details." }
       end
     end
   
     class GooglePlacesApi
       def initialize(api_key)
+        puts "9"
         @api_key = api_key
       end
   
       def get_place_details(place_id)
+        puts "10"
         url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place_id}&key=#{@api_key}"
   
         begin
+          puts "11"
           response = open(url).read
           data = JSON.parse(response)
           puts "*" * 100
@@ -48,17 +55,21 @@ class Api::V1::JobsController < ApplicationController
           puts data.inspect
           puts "*" * 100
           if data['status'] == 'OK'
+            puts "12"
             place_details = data['result']
             # Process and use place_details as needed
             return place_details
           else
+            puts "13"
             puts "Error: #{data['status']}"
             return nil
           end
         rescue OpenURI::HTTPError => e
+          puts "14"
           puts "HTTP Error: #{e.message}"
           return nil
         rescue JSON::ParserError => e
+          puts "15"
           puts "JSON Parsing Error: #{e.message}"
           return nil
         end
