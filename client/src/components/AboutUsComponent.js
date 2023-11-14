1e795ad3edb77970cef9e475aa6b84b4bb6605ab
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-14 11:57:51 -0800

    fixing broken main

--- client/src/components/AboutUsComponent.js
+++ client/src/components/AboutUsComponent.js
@@ -26,7 +26,7 @@ const AboutUsComponent = () => {
                     Recognizing that every patient is unique, Los Angeles Orthopedic Surgery Specialists takes a tailored approach to ensure that each individual fully comprehends their condition and personalized treatment plan. Additionally, we provide preventive and podiatric care to enhance patients' comfort and enable more active lifestyles.
                 </p>
                 <div className='about-right-button'>
-                    <NavLink className='btn header-button-white' alt="Learn More" aria-label="Learn More" to='/about-us'>
+                    <NavLink className='btn header-button-white' alt="Learn More about Los Angeles Orthopedic Specialists" aria-label="Learn More Los Angeles Orthopedic Specialists" to='/about-us'>
                         Learn More
                     </NavLink>
                 </div>
