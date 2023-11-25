7d908f2838206ec8497aad2c206d22bb321f8d79
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-24 18:32:45 -0800

    updating to laorthos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,35 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
   before_action :authenticate_user
-  before_action :set_cors_headers, unless: :api_request?
-  after_action :set_cors_headers, unless: :api_request?
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
   protect_from_forgery with: :null_session
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+  def cors_set_access_control_headers
+    allowed_origins = ['https://www.laorthos.com', 'https://laorthos.com'] # Add any additional allowed origins as needed
+    allowed_methods = 'POST, GET, OPTIONS' # Specify the necessary methods allowed in the request
+    allowed_headers = 'Content-Type, Authorization' # Specify the necessary headers allowed in the request
+
+    headers['Access-Control-Allow-Origin'] = allowed_origins.include?(request.headers['Origin']) ? request.headers['Origin'] : ''
+    headers['Access-Control-Allow-Methods'] = allowed_methods
+    headers['Access-Control-Allow-Headers'] = allowed_headers
+    headers['Access-Control-Max-Age'] = '31536000'
+  end
+
+  def cors_preflight_check
+    return unless request.method == 'OPTIONS'
+
+    allowed_origins = ['https://www.laorthos.com', 'https://laorthos.com'] # Add any additional allowed origins as needed
+    headers['Access-Control-Allow-Origin'] = allowed_origins.include?(request.headers['Origin']) ? request.headers['Origin'] : ''
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '31536000'
+
+    head :ok
+  end
 
   def redirect_to_root
     redirect_to root_path
@@ -34,20 +57,5 @@ class ApplicationController < ActionController::Base
     # Implement your logic to authenticate the user
     # Uncomment the following line if you want to restrict unauthorized access
     # render json: 'Not authorized', status: :unauthorized unless current_user
-  end
-
-  def set_cors_headers
-    allowed_origins = ['https://www.laorthos.com', 'https://laorthos.com']
-    allowed_methods = 'POST, GET, OPTIONS'
-    allowed_headers = 'Content-Type, Authorization'
-
-    headers['Access-Control-Allow-Origin'] = allowed_origins.include?(request.headers['Origin']) ? request.headers['Origin'] : ''
-    headers['Access-Control-Allow-Methods'] = allowed_methods
-    headers['Access-Control-Allow-Headers'] = allowed_headers
-    headers['Access-Control-Max-Age'] = '31536000'
-  end
-
-  def api_request?
-    request.path.start_with?('/api')
   end
 end
