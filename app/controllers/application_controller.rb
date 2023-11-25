dc0a19a9e56f52a8f7cdc5f8edbe2672de248748
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-24 17:56:14 -0800

    updating to laorthos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,28 +8,27 @@ class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
   
     def cors_set_access_control_headers
-      allowed_origins = ['https://www.laorthos.com', 'https://laorthos.com'] # Add both variations
+      allowed_origins = ['https://www.laorthos.com', 'https://laorthos.com'] # Add any additional allowed origins as needed
       allowed_methods = 'POST, GET, OPTIONS' # Specify the necessary methods allowed in the request
       allowed_headers = 'Content-Type, Authorization' # Specify the necessary headers allowed in the request
-  
+    
       headers['Access-Control-Allow-Origin'] = allowed_origins.include?(request.headers['Origin']) ? request.headers['Origin'] : ''
       headers['Access-Control-Allow-Methods'] = allowed_methods
       headers['Access-Control-Allow-Headers'] = allowed_headers
       headers['Access-Control-Max-Age'] = '31536000'
     end
-  
-  
+    
     def cors_preflight_check
       return unless request.method == 'OPTIONS'
-  
-      headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || 'http://laorthos.com'
+    
+      allowed_origins = ['https://www.laorthos.com', 'https://laorthos.com'] # Add any additional allowed origins as needed
+      headers['Access-Control-Allow-Origin'] = allowed_origins.include?(request.headers['Origin']) ? request.headers['Origin'] : ''
       headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
       headers['Access-Control-Allow-Headers'] = '*'
       headers['Access-Control-Max-Age'] = '31536000'
-  
+    
       head :ok
     end
-  
     def redirect_to_root
       redirect_to root_path
     end
