98ba259eaed222b3b079a08920c490138b8480b0
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-24 17:21:19 -0800

    updating to laorthos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
     'Cache-Control' => 'public, max-age=31536000'
   }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "http://la-orthos-bdc751615c67.herokuapp.com"
+  config.asset_host = "http://laorthos.com"
   config.assets.compile = false
 
   # Specifies the header that your server uses for sending files.
