e25fe51c1d5e1b1c71f413d70613228e23ffd698
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-27 12:22:43 -0800

    zocdoc link updated

--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -1,13 +1,16 @@
 class OfficeMailer < ApplicationMailer
     default from: 'unitymskwebsites@gmail.com' # Set your default sender email address
   
-    def contact_us_email(form_data, cc_email)
+    def contact_us_email(form_data)
       @form_data = form_data # Make the form data accessible in the email view template
-      mail(to: 'unitymskwebsites@gmail.com',, cc: cc_email, subject: 'New Contact Form Submission')
+      mail(to: 'unitymskwebsites@gmail.com', subject: 'New Contact Form Submission')
     end
 
-    def request_appointment_email(form_data, cc_email)
+    def request_appointment_email(form_data)
       @form_data = form_data # Make the form data accessible in the email view template
-      mail(to: 'unitymskwebsites@gmail.com',, cc: cc_email, subject: 'New Request Appointment Form Submission')
+      mail(to: 'unitymskwebsites@gmail.com', subject: 'New Request Appointment Form Submission')
     end
   end
+  
+
+  
