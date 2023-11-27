471f14651f42ac530946667a8d11648e5625a719
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-27 13:05:56 -0800

    emailer

--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -1,16 +1,13 @@
 class OfficeMailer < ApplicationMailer
     default from: 'unitymskwebsites@gmail.com' # Set your default sender email address
   
-    def contact_us_email(form_data)
+    def contact_us_email(form_data, cc_email)
       @form_data = form_data # Make the form data accessible in the email view template
-      mail(to: 'unitymskwebsites@gmail.com', subject: 'New Contact Form Submission')
+      mail(to: 'unitymskwebsites@gmail.com', cc: cc_email, subject: 'New Contact Form Submission')
     end
 
-    def request_appointment_email(form_data)
+    def request_appointment_email(form_data, cc_email)
       @form_data = form_data # Make the form data accessible in the email view template
-      mail(to: 'unitymskwebsites@gmail.com', subject: 'New Request Appointment Form Submission')
+      mail(to: 'unitymskwebsites@gmail.com', cc: cc_email, subject: 'New Request Appointment Form Submission')
     end
   end
-  
-
-  
