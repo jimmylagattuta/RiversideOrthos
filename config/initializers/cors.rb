7ae355cc0eef420643d1a8e771ee3c15b6e18dac
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-02-26 02:31:42 -0800

    configuring heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'riversideorthos.azurewebsites.net', 'http://localhost:3001' # Add localhost:3001 to the allowed origins
+    origins 'riversideorthos.azurewebsites.net', 'http://localhost:3001', 'ortho-associates-of-riverside' # Add localhost:3001 to the allowed origins
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
   end
 end
