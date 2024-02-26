66181dadf253124ad27f75bb5d7529921718e993
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-02-26 09:04:46 -0800

    preloading the small medium and large carousel picture

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -68,25 +68,24 @@ const ReactBackgroundCarousel = ({ children }) => {
                     <div
                         className={counter - 1 === index ? 'show' : 'not-show'}
                         key={index}>
-                        {loadedImages.includes(item.props.src) ? (
-                            <img
-                                src={
-                                    (windowWidth <= 1050 && windowWidth > 850 && item.props.src.endsWith('.webp'))
-                                        ? `${item.props.src.replace('.webp', 'l.webp')}`
-                                        : (windowWidth <= 850 && windowWidth > 450 && item.props.src.endsWith('.webp'))
-                                            ? `${item.props.src.replace('.webp', 'm.webp')}`
-                                            : (windowWidth <= 450 && windowWidth > 350 && item.props.src.endsWith('.webp'))
-                                                ? `${item.props.src.replace('.webp', 's.webp')}`
-                                                : (windowWidth <= 350 && item.props.src.endsWith('.webp'))
-                                                    ? `${item.props.src.replace('.webp', 's.webp')}`
-                                                    : item.props.src
-                                }
-                                alt={item.props.alt}
-                                className='carousel-img'
-                            />
-                        ) : (
-                            item
-                        )}
+                       {loadedImages.includes(item.props.src) ? (
+                    <img
+                        src={
+                            (windowWidth <= 1050)
+                                ? `${item.props.src.replace('.webp', 'l.webp')}`
+                                : (windowWidth <= 850)
+                                    ? `${item.props.src.replace('.webp', 'm.webp')}`
+                                    : (windowWidth)
+                                        ? `${item.props.src.replace('.webp', 's.webp')}`
+                                        : item.props.src
+                        }
+                        alt={item.props.alt}
+                        className='carousel-img'
+                        loading={index === 0 ? 'eager' : 'lazy'}
+                    />
+                ) : (
+                    item
+                )}
 
 
 
