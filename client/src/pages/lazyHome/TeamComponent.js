71fe997faf1e36d1bf7b10677729ec23e5a7eba7
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-03-01 22:31:49 -0800

    added printer friendly staff overview

--- client/src/pages/lazyHome/TeamComponent.js
+++ client/src/pages/lazyHome/TeamComponent.js
@@ -22,16 +22,35 @@ const TeamComponent = () => {
 
     return (
         <div className='home-team'>
-            <div className='section-content'>
-                <h2 className='follow-us-title'>
-                    Follow Us
-                </h2>
-                <div className='team-icon-container'>
-                    <a aria-label="Link to go to Orthopaedic Associates of Riverside's Facebook page." style={{ color: 'white' }} href='https://www.facebook.com/orthoriverside' target='_blank' rel='noopener noreferrer'>
-                        <i className='fab fa-facebook fa-3x'></i>
-                    </a>
+
+                <div className="team-inner-div">
+                    <div className="team-second-inner-div">
+                        <h2 className='follow-us-title'>
+                            Follow Us
+                        </h2>
+                        <div className='team-icon-container'>
+                            <a aria-label="Link to go to Orthopaedic Associates of Riverside's Facebook page." style={{ color: 'white' }} href='https://www.facebook.com/orthoriverside' target='_blank' rel='noopener noreferrer'>
+                                <i className='fab fa-facebook fa-3x'></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="team-second-inner-div">
+                        <h2 className='follow-us-title'>
+                            Meet Our Team
+                        </h2>
+                        <div className='team-icon-container'>
+                            <a
+                                className='team-print-staff'
+                                href={process.env.PUBLIC_URL + '/PrintFriendlyStaff.pdf'}
+                                target="_blank" // This opens the PDF in a new tab
+                                rel="noopener noreferrer" // Recommended for security when using target="_blank"
+                            >
+                                Printer Friendly Staff Overview
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
         </div>
     );
 };
--- client/src/pages/physicians/Physicians.js
+++ client/src/pages/physicians/Physicians.js
@@ -4,6 +4,14 @@ import { physicians } from '../../data';
 const Physicians = () => {
     return (
         <div className='page-container'>
+                        <a
+                className='print-staff'
+                href={process.env.PUBLIC_URL + '/PrintFriendlyStaff.pdf'}
+                target="_blank" // This opens the PDF in a new tab
+                rel="noopener noreferrer" // Recommended for security when using target="_blank"
+            >
+                Printer Friendly Staff Overview
+            </a>
             <div className='physician-header'>
                 <h1 className='main-page-title'>Meet Our Physicians</h1>
                 <p className='main-page-description'>
