63015ce90729f36377c3971080a822dfeff3c097
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-04-14 23:35:43 -0700

    updating google places call

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,70 +1,78 @@
 class Api::V1::JobsController < ApplicationController
+  require 'redis'
+  require 'json'
+  require 'uri'
+  require 'net/http'
+
   def index
     render json: "Los Angeles Orthopedic Group " * 1000
   end
 
   def pull_google_places_cache
-    puts "Reached pull_google_places_cache action" # Add this line for debugging
     csrf_token = form_authenticity_token
-    puts "CSRF token: #{csrf_token}" # Add this line for debugging
     place_names = ["Orthopaedic Associates of Riverside", "Orthopaedic Associates of Riverside - La Grange", "Orthopaedic Associates of Riverside - Chicago"]
     reviews = []
 
     place_names.each do |place_name|
-      place_id = find_place_id(place_name)
+      place_id = fetch_place_id_with_caching(place_name)
       if place_id
-        place_reviews = fetch_place_reviews(place_id)
-        five_star_reviews = filter_reviews_by_rating(place_reviews, 5) # Filter by perfect rating (5 stars)
-        filtered_reviews = filter_reviews_by_text(five_star_reviews, 'negative reviews') # Filter out sentences containing "negative reviews" in their text
+        place_reviews = fetch_reviews_with_caching(place_id)
+        five_star_reviews = filter_reviews_by_rating(place_reviews, 5)
+        filtered_reviews = filter_reviews_by_text(five_star_reviews, 'negative reviews')
         reviews.concat(filtered_reviews)
       else
         puts "Place ID not found for '#{place_name}'"
       end
     end
 
-    puts "Retrieved reviews: #{reviews}" # Add this line for debugging
     render json: { reviews: reviews, csrf_token: csrf_token }
   end
 
-
   private
-  require 'redis'
-  require 'json'
-  require 'uri'
-  require 'net/http'
-  def find_place_id(name)
+
+  def fetch_place_id_with_caching(name)
+    redis = Redis.new(url: ENV['REDIS_URL'])
+    cache_key = "place_id_#{name}"
+    cached_id = redis.get(cache_key)
+
+    if cached_id
+      puts "Place ID pulled from cache for '#{name}'"
+      return cached_id
+    end
+
     url = URI("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{URI.encode_www_form_component(name)}&inputtype=textquery&fields=place_id&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
     response = Net::HTTP.get_response(url)
     if response.is_a?(Net::HTTPSuccess)
       data = JSON.parse(response.body)
       if data['candidates'] && !data['candidates'].empty?
-        return data['candidates'][0]['place_id']
+        place_id = data['candidates'][0]['place_id']
+        redis.set(cache_key, place_id, ex: 86400) # Cache for 1 day
+        return place_id
       end
-    else
-      puts "Error occurred while searching for place ID: #{response.code} - #{response.message}"
     end
     nil
   end
 
-  def fetch_place_reviews(place_id)
+  def fetch_reviews_with_caching(place_id)
+    redis = Redis.new(url: ENV['REDIS_URL'])
     cache_key = "google_places_reviews_#{place_id}"
-    cached_reviews = Rails.cache.read(cache_key)
+    cached_reviews = redis.get(cache_key)
+
     if cached_reviews
-      puts "Fetching cached reviews for place_id: #{place_id}"
-      return cached_reviews
-    else
-      puts "Fetching new reviews for place_id: #{place_id}"
-      url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place_id}&fields=reviews&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
-      response = Net::HTTP.get_response(url)
-      if response.is_a?(Net::HTTPSuccess)
-        data = JSON.parse(response.body)
-        if data['result'] && data['result']['reviews']
-          reviews = data['result']['reviews']
-          Rails.cache.write(cache_key, reviews, expires_in: 1.day)
-          return reviews
-        end
-      else
-        puts "Error occurred while fetching place reviews: #{response.code} - #{response.message}"
+      puts "Google Places reviews pulled from cache for place ID: #{place_id}"
+      return JSON.parse(cached_reviews)
+    end
+
+    puts "Calling Google Places API for place ID: #{place_id}"
+
+    url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place_id}&fields=reviews&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
+    response = Net::HTTP.get_response(url)
+    if response.is_a?(Net::HTTPSuccess)
+      data = JSON.parse(response.body)
+      if data['result'] && data['result']['reviews']
+        reviews = data['result']['reviews']
+        redis.set(cache_key, reviews.to_json, ex: 86400) # Cache for 1 day
+        return reviews
       end
     end
     []
