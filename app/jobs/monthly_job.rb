64a166bf8e0c54deda73627cd4bfbc409028da9d
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-04-17 06:23:19 -0700

    scheduler not running because its trying to call the api in the jobs config. switching to perform async to fix timeout bug

--- app/jobs/monthly_job.rb
+++ app/jobs/monthly_job.rb
@@ -28,9 +28,20 @@ class MonthlyJob
       body = response.read_body
       parsed_response = JSON.parse(body)
       if parsed_response['status'] == 'OK'
+        puts "parsed_response['status'] == 'OK'"
+        puts "parsed_response"
+        puts parsed_response.inspect
         place_details = parsed_response['result']
+        puts "place_details"
+        puts place_details.inspect
         place_reviews = place_details.present? ? place_details['reviews'] || [] : []
+        puts "place_reviews"
+        puts place_reviews.inspect
+        puts "reviews before"
+        puts reviews.inspect
         reviews.concat(place_reviews)
+        puts "reviews after"
+        puts reviews.inspect
       else
         puts "Failed to retrieve place details for place ID: #{place_id}"
       end
@@ -39,6 +50,7 @@ class MonthlyJob
     # Filter and process the reviews as needed
     filtered_reviews = []
     reviews.each do |review|
+      puts "a"
       # You can apply filtering or processing logic here
       if review['rating'] == 5 && review['author_name'] != 'Pdub ..'
         filtered_reviews << review
@@ -46,10 +58,14 @@ class MonthlyJob
     end
     redis = Redis.new(url: ENV['REDIS_URL'])
     if redis.exists('cached_google_places_reviews')
+      puts "b"
       redis.del('cached_google_places_reviews')
+      puts "c"
     end
     redis.set('cached_google_places_reviews', JSON.generate(filtered_reviews))
+    puts "d"
     redis.expire('cached_google_places_reviews', 30.days.to_i)
+    puts "e"
   rescue StandardError => e
     puts "Error in monthly_job fetch_google_places_reviews: #{e.message}"
   end
