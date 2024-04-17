9cacd65f22a3f3eb64f6232e9a54c9a9b630112c
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-04-17 05:08:19 -0700

    scheduler not running because its trying to call the api in the jobs config. switching to perform async to fix timeout bug

--- app/jobs/monthly_job.rb
+++ app/jobs/monthly_job.rb
@@ -22,10 +22,20 @@ class MonthlyJob
       # Fetch place details from Google Places API
       encoded_place_id = URI.encode_www_form_component(place_id)
       url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{encoded_place_id}&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
+      puts "url"
+      puts url.inspect
       request = Net::HTTP::Get.new(url)
+      puts "request"
+      puts request.inspect
       response = http.request(request)
+      puts "response"
+      puts response.inspect
       body = response.read_body
+      puts "body"
+      puts body.inspect
       parsed_response = JSON.parse(body)
+      puts "parsed_response"
+      puts parsed_response.inspect
 
       if parsed_response['status'] == 'OK'
         place_details = parsed_response['result']
