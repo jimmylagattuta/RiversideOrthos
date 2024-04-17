d412f7166406aa8b337f54c8fb528f5789c11b63
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-04-17 06:49:26 -0700

    scheduler not running because its trying to call the api in the jobs config. switching to perform async to fix timeout bug

--- app/jobs/monthly_job.rb
+++ app/jobs/monthly_job.rb
@@ -19,7 +19,6 @@ class MonthlyJob
     places.each do |place|
       place_id = place
 
-
       # Fetch place details from Google Places API
       encoded_place_id = URI.encode_www_form_component(place_id)
       url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{encoded_place_id}&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
@@ -28,19 +27,20 @@ class MonthlyJob
       body = response.read_body
       parsed_response = JSON.parse(body)
       if parsed_response['status'] == 'OK'
-        puts "parsed_response['status'] == 'OK'"
-        puts "parsed_response"
+        puts "Successfully fetched place details for place ID: #{place_id}"
+        puts "Parsed response:"
         puts parsed_response.inspect
         place_details = parsed_response['result']
-        puts "place_details"
+        puts "Place details:"
         puts place_details.inspect
         place_reviews = place_details.present? ? place_details['reviews'] || [] : []
-        puts "place_reviews"
+        puts "Place reviews:"
         puts place_reviews.inspect
-        puts "reviews before"
+        puts "Merging reviews..."
+        puts "Reviews before merging:"
         puts reviews.inspect
         reviews.concat(place_reviews)
-        puts "reviews after"
+        puts "Reviews after merging:"
         puts reviews.inspect
       else
         puts "Failed to retrieve place details for place ID: #{place_id}"
@@ -50,7 +50,7 @@ class MonthlyJob
     # Filter and process the reviews as needed
     filtered_reviews = []
     reviews.each do |review|
-      puts "a"
+      puts "Applying review filtering logic..."
       # You can apply filtering or processing logic here
       if review['rating'] == 5 && review['author_name'] != 'Pdub ..'
         filtered_reviews << review
@@ -58,15 +58,17 @@ class MonthlyJob
     end
     redis = Redis.new(url: ENV['REDIS_URL'])
     if redis.exists('cached_google_places_reviews')
-      puts "b"
+      puts "Cached reviews found. Clearing previous cache..."
       redis.del('cached_google_places_reviews')
-      puts "c"
+      puts "Previous cache cleared."
     end
+    puts "Caching filtered reviews..."
     redis.set('cached_google_places_reviews', JSON.generate(filtered_reviews))
-    puts "d"
+    puts "Filtered reviews cached successfully."
+    puts "Setting expiration for cache..."
     redis.expire('cached_google_places_reviews', 30.days.to_i)
-    puts "e"
+    puts "Cache expiration set successfully."
   rescue StandardError => e
-    puts "Error in monthly_job fetch_google_places_reviews: #{e.message}"
+    puts "Error in MonthlyJob: #{e.message}"
   end
 end
