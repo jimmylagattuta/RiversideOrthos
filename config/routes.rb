e19e6dd3dd46d32e89b4ca065edd18373d484a95
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-04-23 19:55:49 -0700

    adding daily visits log

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   namespace :api do
     namespace :v1 do
       resources :notes, only: [:index]
-
       get "/me", to: "users#show"
       post "/signup", to: "users#create"
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
       get "/pull_google_places_cache", to: "jobs#pull_google_places_cache"
       post '/send-email', to: 'email#send_email'
-
     end
   end
 
+  # Existing catch-all for HTML GET requests
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  # New catch-all for all other requests
+  match "*path", to: "fallback#all", via: :all
 end
