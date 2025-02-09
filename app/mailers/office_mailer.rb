b40ebea37e01709b2e3f3deb72d4bc5626f51557
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-08 16:14:13 -0800

    testing emailer

--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -1,14 +1,14 @@
 class OfficeMailer < ApplicationMailer
-  default from: 'unitymskwebsites@gmail.com' # Set your default sender email address
+  default from: 'moaoarpages@gmail.com' # Set your default sender email address
 
   def contact_us_email(form_data, cc_emails)
     @form_data = form_data # Make the form data accessible in the email view template
-    mail(to: 'unitymskwebsites@gmail.com', cc: cc_emails, subject: 'OAR: New Contact Form Submission')
+    mail(to: 'moaoarpages@gmail.com', cc: cc_emails, subject: 'OAR: New Contact Form Submission')
   end
 
   def request_appointment_email(form_data, cc_emails)
     @form_data = form_data # Make the form data accessible in the email view template
-    mail(to: 'unitymskwebsites@gmail.com', cc: cc_emails, subject: 'OAR: New Request Appointment Form Submission')
+    mail(to: 'moaoarpages@gmail.com', cc: cc_emails, subject: 'OAR: New Request Appointment Form Submission')
   end
 
   def alert_no_reviews_email
